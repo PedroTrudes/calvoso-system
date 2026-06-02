ab42eb04b580f345722d8ddde07286702c1a3338
PedroTrudes <pedro.jtrudes@gmail.com>
2026-06-02 16:59:05 -0300

    feat(order-item): criando os endpoints de CRUD

--- calvozo-api/src/services/OrderItemService.js
+++ calvozo-api/src/services/OrderItemService.js
@@ -1,9 +1,30 @@
 const prisma = require("../config/prisma");
 
+async function create(dt) {
+    return await prisma.order_item.create({dt});
+}
+
 async function getAll() {
     return await prisma.order_item.findMany();
 }
 
+async function getById(id) {
+    return await prisma.order_item.findUnique({where: { id : id }})
+}
+
+async function update(id, dt) {
+    return await prisma.order_item.update({where: {id: id},
+    data: {...dt}});
+}
+
+async function deleteById(id) {
+    return await prisma.order_item.delete({where: {id: id}})
+}
+
 module.exports = {
-    getAll
+    create,
+    getAll,
+    getById,
+    update,
+    deleteById
 }
